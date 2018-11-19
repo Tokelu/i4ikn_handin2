dea3873d82f886613dc772d56267e2d26707cdb6
Tonni Lutze <tonni@witzell.dk>
2018-11-19 10:51:42 +0100

    Created serial port connection

--- Link/Link.cs
+++ Link/Link.cs
@@ -15,55 +15,52 @@ namespace Linklaget
 		/// The DELIMITER for slip protocol.
 		/// </summary>
 		const byte DELIMITER = (byte)'A';
-		/// <summary>
-		/// The buffer for link.
-		/// </summary>
-		private byte[] buffer;
-		/// <summary>
-		/// The serial port.
-		/// </summary>
-		SerialPort serialPort;
+        /// <summary>
+        /// The buffer for link.
+	    /// </summary>
+	    private readonly byte[] buffer;
+        /// <summary>
+        /// The serial port.
+        /// </summary>
+        private static SerialPort serialPort = null;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="link"/> class.
+        /// </summary>
+        
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="link"/> class.
-		/// </summary>
-		public Link (int BUFSIZE, string APP)
-		{
-			// Create a new SerialPort object with default settings.
-			#if DEBUG
-				if(APP.Equals("FILE_SERVER"))
-				{
-					serialPort = new SerialPort("/dev/ttySn0",115200,Parity.None,8,StopBits.One);
-				}
-				else
-				{
-					serialPort = new SerialPort("/dev/ttySn1",115200,Parity.None,8,StopBits.One);
-				}
-			#else
-				serialPort = new SerialPort("/dev/ttyS1",115200,Parity.None,8,StopBits.One);
-			#endif
-			if(!serialPort.IsOpen)
-				serialPort.Open();
 
-			buffer = new byte[(BUFSIZE*2)];
+	    public Link(int buffSize)
+	    {
+	        // Create a new SerialPort object with default settings.
 
-			// Uncomment the next line to use timeout
-			//serialPort.ReadTimeout = 500;
+	        serialPort = serialPort ?? new SerialPort("/dev/ttyS1", 115200, Parity.None, 8, StopBits.One);
 
-			serialPort.DiscardInBuffer ();
-			serialPort.DiscardOutBuffer ();
-		}
+	        if (!serialPort.IsOpen)
+	            serialPort.Open();
 
-		/// <summary>
-		/// Send the specified buf and size.
-		/// </summary>
-		/// <param name='buf'>
-		/// Buffer.
-		/// </param>
-		/// <param name='size'>
-		/// Size.
-		/// </param>
-		public void send (byte[] buf, int size)
+	        //  Vi har delimiters også, giver plads til 2 yderligere bytes. 
+	        buffer = new byte[(buffSize * 2) + 2];
+
+	        // Uncomment the next line to use timeout
+	        serialPort.ReadTimeout = 500;
+
+	        serialPort.DiscardInBuffer();
+	        serialPort.DiscardOutBuffer();
+	    }
+
+
+
+
+        /// <summary>
+        /// Send the specified buf and size.
+        /// </summary>
+        /// <param name='buf'>
+        /// Buffer.
+        /// </param>
+        /// <param name='size'>
+        /// Size.
+        /// </param>
+        public void send (byte[] buf, int size)
 		{
 	    	// TO DO Your own code
 		}
